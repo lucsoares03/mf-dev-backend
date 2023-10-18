f88bf44f6c0eac337fb6ac30e41a0c3113e9bc6c
lucsoares03 <lucsoares03@gmail.com>
2023-10-17 22:23:51 -0300

    Configuração do banco de dados e da tabela Transação

---
+++ mf-dev-backend-2023/Models/Transacao.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2023.Models
+{
+    [Table ("Transacoes")]
+    public class Transacao
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o tipo!")]
+        public string Tipo { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o valor!")]
+        public float Valor { get; set; }
+    }
+}
